6990a22ea48bc7382d73bc75b07c12b0d26ff9ec
allwyn46 <allwyn153@gmail.com>
2026-09-08 20:13:03 +0530

    Added HomePage.tsx

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,35 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+const HomePage = () => {
+  return (
+    <section>
+      <div className="flex h-screen items-center justify-center">
+        <div className="w-100">
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+              <CardAction>Card Action</CardAction>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HomePage
